14ea07493758a67753bb1ae91b2a0bb07b38e86b
danzo0l <danzo0l.in@gmail.com>
2022-08-04 23:26:26 -0700

    deploy

--- src/urlsConfig.js
+++ src/urlsConfig.js
@@ -1,3 +1,6 @@
 export const urlsConfig = {
     server_url: "https://express-blog-danzo0l.herokuapp.com",
+    // put url without last slash. 
+    // YES: http://hosting.app
+    // NO:  http://hosting.app/
 };
