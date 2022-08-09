241f515e1ebc927ded3f9f1fc8f3c370e705cc3e
danzo0l <dryagindenisrus@gmail.com>
2022-08-09 08:28:27 +0300

    fix uploads

--- src/urlsConfig.js
+++ src/urlsConfig.js
@@ -1,6 +1,6 @@
 export const urlsConfig = {
-    //server_url: "https://express-blog-danzo0l.herokuapp.com",
-    server_url: "http://127.0.0.1:8000",
+    server_url: "https://express-blog-danzo0l.herokuapp.com",
+    //server_url: "http://127.0.0.1:8000",
     // put url without last slash. 
     // YES: http://hosting.app
     // NO:  http://hosting.app/
